e94f4e29c97b1a2d520898e4936ea850616445ab
Vo Minh Thu <thu@hypered.io>
2021-12-08 15:39:53 +0100

    Add root password and autologin
    Mmm stty is not in the path ?

--- vm-nogui.nix
+++ vm-nogui.nix
@@ -4,18 +4,21 @@
 let
   # https://unix.stackexchange.com/questions/16578/resizable-serial-console-window
   resize = pkgs.writeScriptBin "resize" ''
-    old=$(stty -g)
-    stty raw -echo min 0 time 5
+    old=$(/run/current-system/sw/bin/stty -g)
+    /run/current-system/sw/bin/stty raw -echo min 0 time 5
     printf '\033[18t' > /dev/tty
     IFS=';t' read -r _ rows cols _ < /dev/tty
-    stty "$old"
-    stty cols "$cols" rows "$rows"
+    /run/current-system/sw/bin/stty "$old"
+    /run/current-system/sw/bin/stty cols "$cols" rows "$rows"
   '';
 in {
   virtualisation.memorySize = 1024;
   virtualisation.graphics = false;
   virtualisation.qemu.options = [ "-serial mon:stdio" ];
 
-  environment.systemPackages = [ resize ];
+  environment.systemPackages = [ resize pkgs.coreutils ]; # coreutils for stty
   environment.loginShellInit = "${resize}/bin/resize";
+
+  users.users.root.password = "nixos";
+  services.getty.autologinUser = pkgs.lib.mkDefault "root";
 }
